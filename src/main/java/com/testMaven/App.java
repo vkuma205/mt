1781804f73a14aaefcb4ea017d4dc0f9dc054545
vkuma205 <vinodkumar.gardas@gmail.com>
2017-09-26 20:47:38 -0500

    Initial commit

---
+++ src/main/java/com/testMaven/App.java
@@ -0,0 +1,13 @@
+package com.testMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
